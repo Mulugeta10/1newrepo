4d7aae153c9b40f434fb87f841f1f5dcb3b05c6c
Mulugeta10 <40639993+Mulugeta10@users.noreply.github.com>
2022-07-16 17:48:11 -0400

    Example 2 commit from Eclipse

---
+++ JavaSampleProject/src/com/craft/Example1.java
@@ -0,0 +1,13 @@
+package com.craft;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}
+
+}
